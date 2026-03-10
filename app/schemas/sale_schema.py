1ebc97d608c48aa78d5350af718d8650259a4e3e
Thiago Zanuz <thiagozanuz923@gmail.com>
2026-03-10 10:27:18 -0300

    Add Product and Sale schemas

---
+++ app/schemas/sale_schema.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class SaleCreate(BaseModel):
+    product_id: int
+    quantity: int
+    date: datetime
+
+class SaleResponse(BaseModel):
+    id: int
+    product_id: int
+    quantity: int
+    date: datetime
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
