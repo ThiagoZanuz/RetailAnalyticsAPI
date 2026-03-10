13aad20711ca199d9730d28feaa8783622fcb348
Thiago Zanuz <thiagozanuz923@gmail.com>
2026-03-10 10:49:13 -0300

    Add Sale routes

---
+++ app/api/routes_sales.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from app.database.session import db_dependency
+from app.models.sale import Sale
+from app.schemas.sale_schema import SaleCreate, SaleResponse
+
+router = APIRouter()
+
+@router.post('/sales', response_model=SaleResponse)
+def create_sale(sale: SaleCreate, db: db_dependency):
+    db_sale = Sale(product_id=sale.product_id, quantity=sale.quantity, date=sale.date)
+    db.add(db_sale)
+    db.commit()
+    db.refresh(db_sale)
+    return db_sale
+
+@router.get('/sales', response_model=list[SaleResponse])
+def get_sales(db: db_dependency):
+    return db.query(Sale).all()
